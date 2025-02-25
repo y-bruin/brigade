8d9b178e794f6c2d1f7537afbedaeff6e6e5932e
y-bruin <yuvraj.yadav@getbruin.com>
2025-02-26 02:44:08 +0530

    init proto

--- pkg/plugins/docker/docker.go
+++ pkg/plugins/docker/docker.go
@@ -2,8 +2,14 @@ package docker
 
 import (
 	"context"
+	"fmt"
+	"os"
 
 	common "brigade/internal/gen/common/v1"
+
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/client"
+	"github.com/docker/docker/pkg/stdcopy"
 )
 
 const (
@@ -11,12 +17,32 @@ const (
 	Version    = "v1"
 )
 
-type DockerPluginServer struct{}
+type DockerPluginServer struct {
+	client *client.Client
+}
+
+func NewDockerPluginServer() *DockerPluginServer {
+	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	if err != nil {
+		panic(err)
+	}
+
+	return &DockerPluginServer{
+		client: cli,
+	}
+}
 
 func (e *DockerPluginServer) Execute(
 	ctx context.Context,
 	request *common.ExecuteRequest,
 ) (*common.ExecuteResponse, error) {
+	container, err := e.client.ContainerCreate(ctx, &container.Config{
+		Image: "ubuntu:latest",
+	}, nil, nil, nil, "test-container")
+	if err != nil {
+		return nil, err
+	}
+	fmt.Println(container.ID)
 	return &common.ExecuteResponse{}, nil
 }
 
@@ -24,6 +50,17 @@ func (e *DockerPluginServer) Logs(
 	ctx context.Context,
 	request *common.LogsRequest,
 ) (*common.LogsResponse, error) {
+	reader, err := e.client.ContainerLogs(ctx, "test-container", container.LogsOptions{
+		ShowStdout: true,
+		ShowStderr: true,
+	})
+	if err != nil {
+		return nil, err
+	}
+	defer reader.Close()
+
+	stdcopy.StdCopy(os.Stdout, os.Stderr, reader)
+
 	return &common.LogsResponse{}, nil
 }
 
@@ -31,12 +68,20 @@ func (e *DockerPluginServer) Status(
 	ctx context.Context,
 	request *common.StatusRequest,
 ) (*common.StatusResponse, error) {
-	return &common.StatusResponse{}, nil
+	container, err := e.client.ContainerInspect(ctx, "test-container")
+	if err != nil {
+		return nil, err
+	}
+
+	return &common.StatusResponse{
+		Status: container.State.Status,
+	}, nil
 }
 
 func (e *DockerPluginServer) Events(
 	ctx context.Context,
 	request *common.EventsRequest,
 ) (*common.EventsResponse, error) {
+
 	return &common.EventsResponse{}, nil
 }
